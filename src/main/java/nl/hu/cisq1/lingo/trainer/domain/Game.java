bf374c47ea454adc8a8bd0226f20dafd86db2709
Sterk <lars.sterk@gmail.com>
2021-03-31 11:03:18 +0200

    Added JPA to Game

--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/Game.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/Game.java
@@ -1,9 +1,33 @@
 package nl.hu.cisq1.lingo.trainer.domain;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     private int score;
+//    @OneToMany(mappedBy = "game")
+//    private List<Round> roundList = new ArrayList<>();
+
+    public Game() {
+    }
 
     public Game(int score) {
         this.score = 0;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
     }
 }
