760cf18bddd58dccf4c9a32e37fee7d83c344fbf
Sterk <lars.sterk@gmail.com>
2021-03-31 11:06:23 +0200

    Added SpringGameRepository, data layer

---
+++ src/main/java/nl/hu/cisq1/lingo/trainer/data/SpringGameRepository.java
@@ -0,0 +1,16 @@
+package nl.hu.cisq1.lingo.trainer.data;
+
+import nl.hu.cisq1.lingo.trainer.domain.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SpringGameRepository extends JpaRepository<Game, Long> {
+
+    @Override
+    List<Game> findAll();
+
+    @Override
+    List<Game> findAllById(Iterable<Long> iterable);
+
+}
