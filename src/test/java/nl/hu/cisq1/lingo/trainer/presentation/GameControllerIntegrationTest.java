0ac70fe3813ea4d99e4befbe77f25a621931fb0c
Sterk <lars.sterk@gmail.com>
2021-04-11 21:11:19 +0200

    Added guess integration test

--- src/test/java/nl/hu/cisq1/lingo/trainer/presentation/GameControllerIntegrationTest.java
+++ src/test/java/nl/hu/cisq1/lingo/trainer/presentation/GameControllerIntegrationTest.java
@@ -1,23 +1,41 @@
 package nl.hu.cisq1.lingo.trainer.presentation;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+import javassist.NotFoundException;
 import nl.hu.cisq1.lingo.CiTestConfiguration;
+import nl.hu.cisq1.lingo.trainer.data.SpringGameRepository;
+import nl.hu.cisq1.lingo.trainer.domain.Game;
+import nl.hu.cisq1.lingo.trainer.presentation.DTO.GuessDTO;
 import nl.hu.cisq1.lingo.words.application.WordService;
 import org.junit.jupiter.api.*;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
+import static org.mockito.Mockito.when;
+
 
 @SpringBootTest
+@Transactional
 @Import(CiTestConfiguration.class)
 @AutoConfigureMockMvc
 class GameControllerIntegrationTest {
@@ -34,8 +52,9 @@ class GameControllerIntegrationTest {
     Long game_id;
 
     @BeforeEach
-    void beforeEach() {
+    void beforeEach(){
         game_id = gameController.startNewGame().getGame_id();
+
     }
 
     @Test
@@ -54,6 +73,7 @@ class GameControllerIntegrationTest {
     @Test
     @DisplayName("Test to start a new round")
     void startNewRound() throws Exception {
+        startNewGame();
         RequestBuilder requestBuilder = MockMvcRequestBuilders
                 .post("/lingoTrainer/" + game_id + "/startRound");
 
@@ -63,25 +83,23 @@ class GameControllerIntegrationTest {
                 .andExpect(jsonPath("$.gameState", is("IN_GAME")));
     }
 
-//    @Test
-//    @DisplayName("Test to make a guess")
-//    void makeGuess() throws Exception {
-//
-//        GuessDTO guessDTO = new GuessDTO();
-//        guessDTO.attempt = "STERK";
-//        String body = new ObjectMapper().writeValueAsString(guessDTO);
-//
-//
-//
-//        RequestBuilder requestBuilder = MockMvcRequestBuilders
-//                .post("/lingoTrainer/"+ game_id +"/guess")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(body);
-//
-//        mockMvc.perform(requestBuilder)
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.id", notNullValue()));
-//
-//
-//    }
+    @Test
+    @DisplayName("Test to make a valid guess")
+    void makeGuess() throws Exception {
+        startNewGame();
+        startNewRound();
+
+        GuessDTO guessDTO = new GuessDTO();
+        guessDTO.attempt = "STERK";
+        String body = new ObjectMapper().writeValueAsString(guessDTO);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/lingoTrainer/"+ game_id +"/guess")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(body);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.game_id", notNullValue()));
+    }
 }
